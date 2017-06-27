b7f0ecb6ed8832459b41fc45b7708b5067187e83
powerex <flaxazbest@gmail.com>
2017-06-27 11:56:48 +0300

    check

--- src/main/java/ua/azbest/App.java
+++ src/main/java/ua/azbest/App.java
@@ -6,8 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.InputStream;
-import java.util.Properties;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class App extends Application {
 
@@ -18,12 +18,29 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
         String fxmlFile = "../../main.fxml";
         String styleFile = "../../style.css";
+
+        //ResourceBundle bundle = ResourceBundle.getBundle("UIResources.properties", new Locale("ua"));
         FXMLLoader loader = new FXMLLoader();
-        Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+        //Parent root = (Parent) loader.load(getClass().getResource(fxmlFile), bundle);
+        Parent root = (Parent) loader.load(getClass().getResource(fxmlFile));
         primaryStage.setTitle("Measurement");
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         scene.getStylesheets().add(App.class.getResource(styleFile).toExternalForm());
+
+        /*Properties lang = new Properties();
+        String filename = "lang/ua/lang.properties";
+        try (InputStream input = App.class.getClassLoader().getResourceAsStream(filename)) {
+            if(input==null){
+                System.out.println("No language file " + filename);
+            }
+            lang.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }*/
+
+
+
         primaryStage.show();
     }
 }
